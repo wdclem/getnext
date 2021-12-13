0c235b3f24a01268cb708c4f5c18721f5d609ec3
Clement Cariou <ccariou@c1r1p9.hive.fi>
2021-12-13 17:35:54 +0200

    should work test max

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ccariou <ccariou@hive.fi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 11:11:20 by ccariou           #+#    #+#             */
-/*   Updated: 2021/12/11 13:46:44 by ccariou          ###   ########.fr       */
+/*   Updated: 2021/12/13 17:34:27 by ccariou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,41 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-#define BUF_SIZE 2
+#define BUF_SIZE 110
 
 
 int	get_next_line(const int fd, char **line)
 {
-	size_t		len;
+//	size_t		len;
 	size_t		ret;
-	int			fill;
+//	int			fill;
 	static char	*result;
 	char		*tmp;
 	char		buf[BUF_SIZE + 1];
-	
-	result = ft_strnew(0);
+
+	//result == NULL ? result = ft_strnew(0) : 0;
+	if (result == NULL)
+		result = ft_strnew(0);
 /* handle fd potential errors simplify to < 3 */
+	//printf("fd = %d\n", fd);
 	if (fd < 0 || !line || BUF_SIZE <= 0)
 		return (-1);
-//	fill = read(fd, buf, BUF_SIZE);
+	//printf("%s\n", result);
+	//fill = read(fd, buf, BUF_SIZE);
 	//if fill == o or == -1
-	printf("fill %i\n", fill);
+	//printf("fill %i\n", fill);
 	/* need an other condition for the loop to stop when finding \n ? or > 0 ? */
-	while(!ft_strchr(buf, '\n') && (ret = read(fd, buf, BUF_SIZE)))
+	while(!ft_strchr(result, '\n') && (ret = read(fd, buf, BUF_SIZE)) > 0)
 	{
-		printf("ret%zu\n", ret);
+	//	printf("ret%zu\n", ret);
 		buf[ret] = '\0';
 		tmp = ft_strjoin(result, buf);
 		/* bzero ? or memdel to make result 2d */
-	 ft_memdel((void **)&result);
+		ft_memdel((void **)&result);
 		result = tmp;
-		printf("%s\n", result);
+		//printf("%s\n", result);
 	}
+	//printf("ret%zu\n", ret);
 	/* if ret 0 line == complete result ? should it check for \n ? */
 	if (ret == 0)
 		*line = ft_strdup(result);
@@ -52,8 +57,19 @@ int	get_next_line(const int fd, char **line)
 	int	index = 0;
 	while (result[index] != '\n' && result[index] != '\0')
 		index++;
+	//printf("index = %i", index);
 	if (ret > 0)
 		*line = ft_strsub(result, 0, index);
+	//printf("line len = %zu", ft_strlen(*line));
+	//result = &result[index + 1];
+	//tmp = &result[index + 1];
+	if (ret > 0)
+		tmp = ft_strdup(result + (index + 1));
+	else
+		tmp = ft_strdup(result + index);
+	ft_memdel((void **)&result);
+	result = tmp; 
+	//free (tmp); ??
 	/* if fill exist duplicate the content */
 //	len = ft_strlen((char *) buf);
 //	printf("%zu\n", len);
@@ -86,19 +102,42 @@ int	get_next_line(const int fd, char **line)
 	if (error)
 		return (-1);
 		*/
-	return (0);
+	printf("ret = %zu\n", ret);
+	if (ret == 0)
+		ft_memdel((void **)&result);
+	return (ret == 0 ? 0 : 1);
 }
 
 int	main()
 {
 	int		fd;
 	int		retget;
-	char	*line = NULL;
+	char	*tmp;
+	char	**line;
+	int		n = 1;
 
-	fd = open("h.txt", O_RDONLY);
-	retget = get_next_line(fd, &line);
-	printf("%i\n%s\n", retget, line);
-	retget = get_next_line(fd, &line);
-	printf("%i\n%s\n", retget, line);
+	line = &tmp;
+	//fd = open("h.txt", O_RDONLY);
+	//get_next_line(fd, &line);
+	//printf("retget = %i\n", retget);
+	//while((get_next_line(fd, &line) > 0))
+	//	printf("%s\n", line);
+	//get_next_line(fd, &line);
+	//printf("%s\n", line);
+	fd = open("otis.txt", O_RDONLY);
+	if (fd < 0)
+		return (0);
+	printf("Two lines with newline:\n");
+	while (1)
+	{
+		retget = get_next_line(fd, line);
+		if (retget <= 0)
+			break;
+		printf("%s<- end of line %d\n", *line, n);
+		free(*line);
+		n++;
+	}
+	close (fd);
+	//system("leaks a.out");
 	return(0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,16 @@
 /*   By: ccariou <ccariou@hive.fi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 11:11:54 by ccariou           #+#    #+#             */
-/*   Updated: 2021/12/07 11:12:01 by ccariou          ###   ########.fr       */
+/*   Updated: 2021/12/13 09:55:15 by ccariou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+//# define BUFF_SIZE 1
+
+#include "libft/libft.h"
+
+int	get_next_line(const int fd, char **line);
+
+#endif
