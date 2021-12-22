94a88a251224b6896f5a99fa0639ad289a5403e6
Clément C <32699739+wdclem@users.noreply.github.com>
2021-12-22 12:56:48 +0200

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -1,75 +1,76 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   clabone.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ccariou <ccariou@hive.fi>                  +#+  +:+       +#+        */
+/*   By: ccariou <ccariou@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/16 09:54:13 by ccariou           #+#    #+#             */
-/*   Updated: 2021/12/16 19:57:42 by ccariou          ###   ########.fr       */
+/*   Created: 2021/12/21 21:21:34 by ccariou           #+#    #+#             */
+/*   Updated: 2021/12/22 12:52:27 by ccariou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
 #include <stdio.h>
+#include "get_next_line.h"
 
-int	ft_copyline(char **result, char **line)
+static int	set_dup(char **dup, char *tmp)
 {
-	int		index;
-	char	*tmp;
-
-	index = 0;
-	while (*result[index])
-		index++;
-	printf("ret = %i\n", index);
-	if (*result[index] == '\n')
-	{
-		*line = ft_strsub(*result, 0, index);
-		tmp = ft_strdup(&(*result)[index + 1]);
-		ft_memdel((void **)&result);
-		*result = tmp;
-		if ((*result)[0] == '\0')
-			ft_strdel(result);
-	}
-	else
-	{
-		*line = ft_strdup(*result);
-		ft_strdel(result);
-	}
+	ft_strdel(dup);
+	*dup = ft_strdup(tmp);
+	ft_strdel(&tmp);
 	return (1);
 }
 
-int	ft_return(char **result, char **line, size_t ret, int fd)
+static int	ft_read(char **dup, const int fd, char *tmp)
 {
-	if (ret < 0)
-		return (-1);
-	else if (ret == 0 && !result[fd])
+	char	buf[BUFF_SIZE + 1];
+	int		bytes;
+
+	bytes = read(fd, buf, BUFF_SIZE);
+	if (bytes == 0)
 		return (0);
-	return(ft_copyline(&result[fd], line));
+	buf[bytes] = '\0';
+	tmp = ft_strjoin(dup[fd], buf);
+	set_dup(&dup[fd], tmp);
+	return (1);
+}
+
+static int	ft_single_line(char **dup, char **line)
+{
+	*line = ft_strdup(*dup);
+	ft_strclr(*dup);
+	return (1);
+}
+
+static int	ft_outuput(char **dup, char **line, char *tmp, char *ptr)
+{
+	if (!(ft_strchr(*dup, '\n')))
+		return (ft_single_line(dup, line));
+	tmp = ft_strdup(ptr + 1);
+	*line = ft_strsub(*dup, 0, (ptr - *dup));
+	return (set_dup(dup, tmp));
 }
 
 int	get_next_line(const int fd, char **line)
 {
-	size_t		ret;
-	static char	*result[FD_SIZE];
-	char		*tmp;
 	char		buf[BUFF_SIZE + 1];
+	static char	*dup[FD_SIZE];
+	char		*tmp;
+	char		*ptr;
 
-	if (fd < 0 || !line || read(fd, buf, 0) < 0 || BUFF_SIZE <= 0)
+	tmp = NULL;
+	if (fd < 0 || line == NULL || read(fd, buf, 0) < 0)
 		return (-1);
-	if (!result[fd])
-		result[fd] = ft_strnew(0);
-	ret = 1;
-	while (!ft_strchr(result[fd], '\n') && ret > 0)
+	if (!dup[fd])
+		dup[fd] = ft_strnew(0);
+	ptr = ft_strchr(dup[fd], '\n');
+	while (ptr == NULL)
 	{
-		ret = read(fd, buf, BUFF_SIZE);
-		printf("ret%zu\n", ret);
-		buf[ret] = '\0';
-		tmp = ft_strjoin(result[fd], buf);
-		printf("%s\n", tmp);
-		ft_memdel((void **)&result);
-		result[fd] = tmp;
-		printf("%s\n", *result);
+		if ((ft_read(dup, fd, tmp)) == 0)
+			break ;
+		ptr = ft_strchr(dup[fd], '\n');
 	}
-	return(ft_return(result, line, ret, fd));
+	if (ft_strlen(dup[fd]) != 0)
+		return (ft_outuput (&dup[fd], line, tmp, ptr));
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,19 +6,17 @@
 /*   By: ccariou <ccariou@hive.fi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 11:11:54 by ccariou           #+#    #+#             */
-/*   Updated: 2021/12/16 14:34:47 by ccariou          ###   ########.fr       */
+/*   Updated: 2021/12/22 12:43:25 by ccariou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 2 
-# define FD_SIZE 256
+
+# define BUFF_SIZE 10 
+# define FD_SIZE 256 
 
 # include "libft/libft.h"
-# include <fcntl.h>
-# include <sys/types.h>
-# include <sys/uio.h>
 
 int	get_next_line(const int fd, char **line);
 
