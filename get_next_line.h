db5e4ba129e1aa1d121761fea7639194afbb4b9d
Clément C <32699739+wdclem@users.noreply.github.com>
2022-01-04 11:52:03 +0200

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccariou <ccariou@hive.fi>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/07 11:11:54 by ccariou           #+#    #+#             */
+/*   Updated: 2021/12/29 10:15:44 by ccariou          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 1
+# define FD_SIZE 256
+
+# include "libft/libft.h"
+
+int	get_next_line(const int fd, char **line);
+
+#endif
